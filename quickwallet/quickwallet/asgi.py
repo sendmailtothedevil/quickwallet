2c7f6f6e97e16a4b4c28c0c3bd33ab3d9ff5e373
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-04 08:01:52 -0800

    change

--- quickwallet/quickwallet/asgi.py
+++ quickwallet/quickwallet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quickwallets project.
+ASGI config for quickwallet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallets.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallet.settings')
 
 application = get_asgi_application()
--- quickwallet/quickwallet/wsgi.py
+++ quickwallet/quickwallet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quickwallets project.
+WSGI config for quickwallet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallets.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallet.settings')
 
 application = get_wsgi_application()
