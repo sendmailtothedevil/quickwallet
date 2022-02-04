0d703f744e7c08c41d15dd24ea1c355b0397abce
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-04 07:06:30 -0800

    kill

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('wallets/<slug:slug>/', views.wallets, name='wallets'),
-    path('<slug:slug>/', views.wallet_details, name='wallet_details'),
-]
