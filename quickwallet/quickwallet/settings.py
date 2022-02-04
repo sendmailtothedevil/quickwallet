314320168c1155f80c34925e979040ba8a61154a
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-04 15:14:45 -0800

    make it better

--- quickwallet/quickwallet/settings.py
+++ quickwallet/quickwallet/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'quickwallet.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER': os.environ.get('DB_USER'),
+    #     'PASSWORD': os.environ.get('DB_PASSWORD'),
+    #     'HOST': os.environ.get('DB_HOST'),
+    #     'PORT': 5432
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': 'wc',
+        'USER': 'postgres',
+        'PASSWORD': 'awaays@7513596980#',
+        'HOST': 'localhost',
         'PORT': 5432
     }
 }
