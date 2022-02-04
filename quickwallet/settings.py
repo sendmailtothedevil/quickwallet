441f1745f8262b71e88056cc6094690ae66866f1
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-04 03:52:30 -0800

    miol

--- quickwallet/settings.py
+++ quickwallet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1' #1 == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quickwallet.herokuapp.com']
 
 
 # Application definition
