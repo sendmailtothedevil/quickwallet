776f2d15064731f910b9e6f9a6efd322ce18891a
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-04 16:10:39 -0800

    hun

--- quickwallet/quickwallet/settings.py
+++ quickwallet/quickwallet/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vxm+785#clcdeh$#gr=e%s9=kbwove+i3doz8iv#(vkzl9)xw_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quickwallet.herokuapp.com']
 
 
 # Application definition
@@ -139,8 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sendmailtothedevil@gmail.com'
-EMAIL_HOST_PASSWORD = 'oyltfkrflczluhig'
+
