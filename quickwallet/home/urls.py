31179081f9fffda0f5b188520764b94b3d62b5d8
awaays <awaays@awaays.com>
2022-02-24 18:57:59 -0500

    make it better

--- quickwallet/home/urls.py
+++ quickwallet/home/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('wallets/<slug:slug>/', views.wallets, name='wallets'),
     path('<slug:slug>/', views.wallet_details, name='wallet_details'),
 ]
+
+# ad32891d9e98f06b691298bf5189b71d1289bd9d268dc6e9
