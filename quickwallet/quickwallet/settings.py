5385df4ebb4e803356d99dbf2dfe80c56ebdfc98
awaays <awaays@awaays.com>
2022-02-24 20:51:41 -0500

    move

--- quickwallet/quickwallet/settings.py
+++ quickwallet/quickwallet/settings.py
@@ -128,12 +128,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'jttqvvxporibkobo'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = 'jttqvvxporibkobo'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 
 django_heroku.settings(locals())
