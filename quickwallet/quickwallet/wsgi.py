59c6ea7b55ca937f858efaf13cd0f6cda6b052f1
awaays <awaays@awaays.com>
2022-02-24 19:07:26 -0500

    make it better

--- quickwallet/quickwallet/wsgi.py
+++ quickwallet/quickwallet/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickwallets.settings')
 
 application = get_wsgi_application()
