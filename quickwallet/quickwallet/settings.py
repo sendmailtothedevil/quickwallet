a5755ef661bf0a9862bc513a2a0265ad6f9e5f5c
awaays <awaays@awaays.com>
2022-02-24 19:57:40 -0500

    move

--- quickwallet/quickwallet/settings.py
+++ quickwallet/quickwallet/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'jttqvvxporibkobo'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
